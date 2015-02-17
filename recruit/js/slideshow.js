5b800f85e150ccd528bd9af84e609a0422843c35
LeoMao <LMao20001@gmail.com>
2015-02-17 16:49:19 +0800

    [promotion] add slideshow.js, background image slideshow

---
+++ recruit/js/slideshow.js
@@ -0,0 +1,36 @@
+// Author: LeoMao
+
+'use strict';
+
+(function() {
+    // element need to be changed background
+    var banner = document.querySelector("#banner");
+    var cta = document.querySelector("#cta");
+
+    // time step
+    var tstep = 4000;
+    
+    // count and max
+    var MAX = 2;
+    var nowIndex = 0;
+    var images = [
+        "images/banner.jpg",
+        "images/banner0.jpg"
+    ];
+
+    var nextbg = function(element) {
+        var front = element.querySelector(".bg.front");
+        var back = element.querySelector(".bg.back");
+        back.style.backgroundImage = "url('css/images/light-bl.svg'), url('css/images/light-br.svg'), url('css/images/overlay.png'), url('" + images[nowIndex] + "')";
+        front.classList.toggle('front');
+        front.classList.toggle('back');
+        back.classList.toggle('back');
+        back.classList.toggle('front');
+    }
+
+    setInterval(function() {
+        nowIndex = (nowIndex + 1) % MAX;
+        nextbg(banner);
+        nextbg(cta);
+    }, tstep);
+})();
